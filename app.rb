0591c876aeb2ee6d2dea0d98b32188fe29c594fd
Luiz Picolo <luizpicolo@gmail.com>
2021-11-03 21:35:11 -0400

    changed name of labels

--- app.rb
+++ app.rb
@@ -9,14 +9,15 @@ Time.now.utc.localtime("-04:00")
 
 get '/' do
   @transfer_rate = [
-    {'name': 'Download Rate', 'data': Rate.transfer('download')},
-    {'name': 'Upload Rate', 'data': Rate.transfer('upload')}
+    {'name': 'Download', 'data': Rate.transfer('download')},
+    {'name': 'Upload', 'data': Rate.transfer('upload')}
   ]
 
   @download_rate = [
-    {'name': 'Max Download Rate', 'data': Rate.extremes(:maximum, :download)},
-    {'name': 'Min Download Rate', 'data': Rate.extremes(:minimum, :download)},
-    {'name': 'Everage Download Rate', 'data': Rate.extremes(:average, :download)}
+    {'name': 'Max', 'data': Rate.extremes(:maximum, :download)},
+    {'name': 'Min', 'data': Rate.extremes(:minimum, :download)},
+    {'name': 'Everage', 'data': Rate.extremes(:average, :download)}
+  ]
 
   @upload_rate = [
     {'name': 'Max', 'data': Rate.extremes(:maximum, :upload)},
