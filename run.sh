6ec522b007d0e2ccce55f9be6006cee23d61d112
Luiz F. Picolo <luizpicolo@gmail.com>
2022-06-15 23:54:34 -0400

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-echo '--- run yarn install'
-yarn --cwd public/
-
-echo '--- run bundle install'
-bundle install
-
 echo '--- create docker image and up it'
 docker-compose up -d --build
+
+echo '--- run yarn install'
+docker-compose run app yarn --cwd public/
 
 echo '--- update cronjob'
 docker-compose run app whenever -w
